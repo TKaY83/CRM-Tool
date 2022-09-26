8aa6279756cb5330bfd4f183c74bc3ad2ebc00d2
Tuncay Dagdelen <sramos083@gmail.com>
2022-09-26 09:05:32 +0200

    user details finished

--- src/app/dialog-edit-address/dialog-edit-address.component.ts
+++ src/app/dialog-edit-address/dialog-edit-address.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { MatDialogRef } from '@angular/material/dialog';
 import { User } from 'src/models/user.class';
 
@@ -9,14 +10,23 @@ import { User } from 'src/models/user.class';
 })
 export class DialogEditAddressComponent implements OnInit {
   user: User;
+  userID: string;
   loading = false;
-  constructor(public dialogRef: MatDialogRef<DialogEditAddressComponent>) { }
+  constructor(public dialogRef: MatDialogRef<DialogEditAddressComponent>, private firestore: AngularFirestore) { }
 
   ngOnInit(): void {
   }
 
   saveUser(){
-
+    this.loading = true;
+    this.firestore
+    .collection('users')
+    .doc(this.userID)
+    .update(this.user.toJSON())
+    .then(()=>{
+      this.loading = false;
+      this.dialogRef.close();
+    });
   }
 
 }
--- src/app/user-detail/user-detail.component.ts
+++ src/app/user-detail/user-detail.component.ts
@@ -39,13 +39,15 @@ export class UserDetailComponent implements OnInit {
 
   editUser(){
     const dialog = this.dialog.open(DialogEditUserComponent);
-    dialog.componentInstance.user = this.user;
+    dialog.componentInstance.user = new User(this.user.toJSON());
+    dialog.componentInstance.userID = this.userID;
 
   }
 
   editAddress(){
     const dialog = this.dialog.open(DialogEditAddressComponent);
-    dialog.componentInstance.user = this.user;
+    dialog.componentInstance.user = new User(this.user.toJSON());
+    dialog.componentInstance.userID = this.userID;
   }
 
 }
